038d9557928cb1b6cf18604dc6b320c57933b472
Kyryl <lerroy.k@icloud.com>
2026-03-17 16:41:14 +0100

    Initial commit, base structure defined

---
+++ APBD_Tutorial _2-CS_Object-Oriented_Project/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace APBD_Tutorial__2_CS_Object_Oriented_Project;
+
+public static class Program {
+    public static void Main(string[] args) {
+        
+    }
+}
---
+++ APBD_Tutorial _2-CS_Object-Oriented_Project/UserService.cs
@@ -0,0 +1,5 @@
+﻿namespace APBD_Tutorial__2_CS_Object_Oriented_Project;
+
+public class UserService {
+    
+}
