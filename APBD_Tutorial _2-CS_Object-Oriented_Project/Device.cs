b28f9fcc40b309e4846ce39765fc171813286786
Kiryu Setsuna <lerroy.k@icloud.com>
2026-03-23 20:47:49 +0100

    Transformed Device class into an abstract with new properties and availability management methods

--- APBD_Tutorial _2-CS_Object-Oriented_Project/Device.cs
+++ APBD_Tutorial _2-CS_Object-Oriented_Project/Device.cs
@@ -1,5 +1,22 @@
 ﻿namespace APBD_Tutorial__2_CS_Object_Oriented_Project;
 
-public class Device {
+public abstract class Device {
+    private static int _id = 1;
     
+    public int Id { get; }
+    public string Name { get; set; }
+    public bool IsAvailable { get; private set; }
+    
+    public Device(String name) {
+        Id = _id++;
+        IsAvailable = true;
+        Name = name;
+    }
+
+    public void SetAvailable() {
+        IsAvailable = true;
+    }
+    public void SetUnavailable() {
+        IsAvailable = false;
+    }
 }
