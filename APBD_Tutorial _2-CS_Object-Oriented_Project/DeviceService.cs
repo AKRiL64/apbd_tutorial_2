ffb61a923291974bad56e29968a7d5c9976a9e5f
Kiryu Setsuna <lerroy.k@icloud.com>
2026-03-23 21:56:32 +0100

    Implemented DeviceService and UserServise for managing devices and users.

--- APBD_Tutorial _2-CS_Object-Oriented_Project/DeviceService.cs
+++ APBD_Tutorial _2-CS_Object-Oriented_Project/DeviceService.cs
@@ -1,5 +1,27 @@
 ﻿namespace APBD_Tutorial__2_CS_Object_Oriented_Project;
 
-public class DeviceService {
+public class DeviceService
+{
+    private readonly List<Device> devices = [];
+
+    public void Add(Device device) {
+        devices.Add(device);
+    }
+
+    public Device GetById(int id) {
+        return devices.Single(d => d.Id == id);
+    }
+
+    public IEnumerable<Device> GetAll() {
+        return devices;
+    }
+
+    public IEnumerable<Device> GetAvailable() {
+        return devices.Where(d => d.IsAvailable);
+    }
     
+    public void MarkUnavailable(int deviceId)
+    {
+        GetById(deviceId).SetUnavailable();
+    }
 }
--- APBD_Tutorial _2-CS_Object-Oriented_Project/UserService.cs
+++ APBD_Tutorial _2-CS_Object-Oriented_Project/UserService.cs
@@ -1,5 +1,17 @@
 ﻿namespace APBD_Tutorial__2_CS_Object_Oriented_Project;
 
 public class UserService {
-    
+    private readonly List<User> users = [];
+
+    public void Add(User user) {
+        users.Add(user);
+    }
+
+    public User GetById(int id) {
+        return users.Single(u => u.Id == id);
+    }
+
+    public IEnumerable<User> GetAll() {
+        return users;
+    }
 }
