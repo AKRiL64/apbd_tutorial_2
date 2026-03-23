7d55155b63ee74e349ffc492e02909433764c050
Kiryu Setsuna <lerroy.k@icloud.com>
2026-03-23 21:02:18 +0100

    Transformed User class into an abstract class with unique ID generation and new properties for FirstName and LastName.

--- APBD_Tutorial _2-CS_Object-Oriented_Project/User.cs
+++ APBD_Tutorial _2-CS_Object-Oriented_Project/User.cs
@@ -1,5 +1,16 @@
 ﻿namespace APBD_Tutorial__2_CS_Object_Oriented_Project;
 
-public class User {
-    
+public abstract class User {
+    private static int _id = 1;
+
+    public int Id { get; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    protected User(string firstName, string lastName)
+    {
+        Id = _id++;
+        FirstName = firstName;
+        LastName = lastName;
+    }
 }
