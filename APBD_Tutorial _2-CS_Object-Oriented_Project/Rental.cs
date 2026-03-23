6bbf7561a2e68f60b0b6f4809a2b180235bab7a6
Kiryu Setsuna <lerroy.k@icloud.com>
2026-03-23 21:42:54 +0100

    Added Rental class with user, device, and rental period management, including return logic with penalty calculation.

--- APBD_Tutorial _2-CS_Object-Oriented_Project/Rental.cs
+++ APBD_Tutorial _2-CS_Object-Oriented_Project/Rental.cs
@@ -1,5 +1,29 @@
 ﻿namespace APBD_Tutorial__2_CS_Object_Oriented_Project;
 
 public class Rental {
+    private const float RENTAL_PENALTY_PER_DAY = 100f;
+    public User User { get; }
+    public Device Device { get; }
+    public DateTime StartDate { get; }
+    public DateTime EndDate { get; }
+    public DateTime? ReturnDate { get; private set; }
     
+    public Rental(User user, Device device, int days) {
+        User = user;
+        Device = device;
+        StartDate = DateTime.Now;
+        EndDate = StartDate.AddDays(days);
+    }
+    
+    public bool isActive() {
+        return ReturnDate == null;
+    }
+    
+    public float Return() {
+        ReturnDate = DateTime.Now;
+        Device.SetAvailable();
+        if (ReturnDate <= EndDate) return 0f;
+        int daysDelay = (ReturnDate.Value - EndDate).Days;
+        return daysDelay * RENTAL_PENALTY_PER_DAY;
+    }
 }
